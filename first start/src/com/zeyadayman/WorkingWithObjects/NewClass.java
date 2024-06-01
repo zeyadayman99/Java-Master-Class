c53ac56c43b6afca7f14eb524373fced5236e97f
Zeyad Ayman <ziadayman095@gmail.com>
2024-06-01 07:19:45 +0300

    message

---
+++ first start/src/com/zeyadayman/WorkingWithObjects/NewClass.java
@@ -0,0 +1,8 @@
+package com.zeyadayman.WorkingWithObjects;
+
+public class NewClass {
+    public static void main(String[] args) {
+        ClassesAndObjects obj = new ClassesAndObjects("rose", 2);
+        System.out.println(obj.getName() + obj.getAge());
+    }
+}
