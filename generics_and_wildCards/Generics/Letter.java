213de6f6f4baa7cd675b59724dfbc79fcb6c26f4
Zeyad Ayman <ziadayman095@gmail.com>
2024-06-27 23:45:18 +0300

    adding Generics exercises

---
+++ generics_and_wildCards/Generics/Letter.java
@@ -0,0 +1,20 @@
+package DiffExamples.ClassesAndObjects.generics_and_wildCards.Generics;
+
+public class Letter {
+    private final String sender;
+
+    public Letter(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Letter{" +
+                "sender='" + sender + '\'' +
+                '}';
+    }
+}
