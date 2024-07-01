78186b53fbc192874553958bd57d6939d369eda6
Zeyad Ayman <ziadayman095@gmail.com>
2024-07-01 07:58:12 +0300

    Committing resolved conflicts and local changes before pull

--- First_Maven_Project/src/test/java/org/example/CalculatorTest.java
+++ First_Maven_Project/src/test/java/org/example/CalculatorTest.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
+
+    @Test
+    void canAddTwoNumbers() {
+        Calculator underTest = new Calculator();
+        int number1 = 10;
+        int number2 = 15;
+        int result = underTest.add(number1, number2);
+        assertEquals(25, result);
+    }
+    @Test
+    void canAcceptZeroAsNumbers() {
+        Calculator underTest = new Calculator();
+        int result = underTest.add(0);
+        assertEquals(0, result);
+    }
+    @Test
+    void canAddNumbersFromAGivenArray() {
+        Calculator underTest = new Calculator();
+        var numbers = new int[]{1, 2, 3};
+        int result = underTest.add(numbers);
+        assertEquals(6, result);
+    }
 }
